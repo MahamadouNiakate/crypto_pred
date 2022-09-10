d9a761e55344535bd22ae3a7ae8658c81f458975
Dominic DiMarco <dominic.dimarco@gmail.com>
2022-09-10 11:41:44 +0200

    base presentation working well.  adding buy/hold gain/loss next

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def calc_with_params(start_date, bet_size):
   gain_percents = [0, .05, .1]
   model_list = [('lr', LinearRegression()),
                 # ('xgb', xgb.XGBRegressor(objective ='reg:squarederror')),
-                ('knn', KNeighborsRegressor(n_neighbors=2)),
+               ('knn', KNeighborsRegressor(n_neighbors=2)),
                 ('rf', RandomForestRegressor()) ]
 
   window_df = btc_month_mean.copy()[start_date:]
@@ -229,7 +229,7 @@ def calc_with_params(start_date, bet_size):
   for window_size in window_sizes:
     for gain_percent in gain_percents:
       for model_name, model in model_list:
-        # print(f'{window_size}:{gain_percent}:{model_name}')
+        # st.write(f'{window_size}:{gain_percent}:{model_name}')
         test_df = calc_sliding_window2(model_name, model, window_size, window_df, False)
 
         test_df = append_gain_loss(month_close_df, test_df)
@@ -250,192 +250,66 @@ def calc_with_params(start_date, bet_size):
         # print(f'gain_loss_bet_size_list: {gain_loss_bet_size_list}')
         gain_loss_bet_size = sum(gain_loss_bet_size_list)
 
-        loop_df = pd.DataFrame(data=[[window_size, gain_percent, model_name, gain_loss, w, l, p, gain_loss_bet_size, w_100, l_100, p_100]],
-                               columns=['window', 'percent', 'model_name', 'gain_loss', 'gl_win', 'gl_loss', 'gl_pass', f'gain_loss_{bet_size}', 'gl_win_100', 'gl_loss_100', 'gl_pass_100'])
+        loop_df = pd.DataFrame(data=[[window_size, gain_percent, model_name, gain_loss,
+                                      w, l, p, gain_loss_bet_size, w_100, l_100, p_100]],
+                               columns=['window', 'percent', 'model_name', 'gain_loss',
+                                        'gl_win', 'gl_loss', 'gl_pass', f'gain_loss_{bet_size}',
+                                        'gl_win_100', 'gl_loss_100', 'gl_pass_100'])
 
         # print(f'-->{result_df.shape}, {loop_df.shape}, {len(gain_loss_bet_size_list)}')
-
         result_df = pd.concat([result_df, loop_df], ignore_index=True)
+
+  result_df["gl_win_100"] = result_df["gl_win_100"].astype(int)
+  result_df["gl_loss_100"] = result_df["gl_loss_100"].astype(int)
+  result_df["gl_pass_100"] = result_df["gl_pass_100"].astype(int)
+  # st.write(f'd result_df.shape: {result_df.shape}')
+
+  result_df = result_df.sort_values(by='gain_loss_100', ascending=False)
+  # st.write(f'e result_df.shape: {result_df.shape}')
+  result_df = result_df[['window', 'percent', 'model_name', 'gain_loss_100', 'gl_win_100', 'gl_loss_100', 'gl_pass_100']]
+  # # st.write(f'f result_df.shape: {result_df.shape}')
+  result_df = result_df.rename(columns={"window": "window", "percent": "threshold", "model_name": "model",
+                                        "gain_loss_100": "gain_loss", "gl_win_100": "win",
+                                        "gl_loss_100": "loss", "gl_pass_100": "pass"})
+  # # st.write(f'g result_df.shape: {result_df.shape}')
+  result_df = result_df.assign(hack='').set_index('hack')
+  # st.write(f'h result_df.shape: {result_df.shape}')
+
   return result_df
 
 '''------------------------------------------------'''
 
-st.write('DEFINE month_ave_close_shift_diff_percent')
-'''
-
-seasonal_decompose(btc_month_mean['month_ave_close_shift_diff_percent']).plot()
-print("Dickey–Fuller test: p=%f" % adfuller(btc_month_mean['month_ave_close_shift_diff_percent'])[1])
-
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
-plot_acf(btc_month_mean['month_ave_close_shift_diff_percent'], lags=24);
-plot_pacf(btc_month_mean['month_ave_close_shift_diff_percent'], lags=24);
-
-'''
-# Plain Linear Regression
-st.write(f'A Starting: {start_date_string}')
-
 btc_month_mean, month_close_df, test, X_train, y_train, X_test, y_test = preprocess(btc)
-predict_df = standard_lr(test, X_train, y_train, X_test, y_test)
-predict_df = append_gain_loss(month_close_df, predict_df)
-
-
-
-#dbd todo
-# seasonal_decompose(btc_month_mean['month_ave_close_shift_diff_percent']).plot()
-# print("Dickey–Fuller test: p=%f" % adfuller(btc_month_mean['month_ave_close_shift_diff_percent'])[1])
-
-# from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
-# plot_acf(btc_month_mean['month_ave_close_shift_diff_percent'], lags=24);
-# plot_pacf(btc_month_mean['month_ave_close_shift_diff_percent'], lags=24);
-
-
-thresholds = [0]#, .1, .2]
-results = []
-for threshold in thresholds:
-
-#   gain_loss = calc_gain_loss(predict_df, threshold, 'Y_PRED')
-#   gain_loss_100 = calc_gain_loss_2(predict_df, threshold, 'Y_PRED', 100, False)
-
-
-  gain_loss_list = calc_gain_loss(predict_df, threshold, 'Y_PRED')
-  l = len(list(filter(lambda x: (x < 0), gain_loss_list)))
-  w = len(list(filter(lambda x: (x > 0), gain_loss_list)))
-  p = len(list(filter(lambda x: (x == 0), gain_loss_list)))
-#   print(f'gain_losslist: {w}/{l}/{p}::{len(gain_loss_list)}::{gain_loss_list}--{predict_df.shape}')
-
-#   print(f'gain_loss_list: {len(gain_loss_list)}::{gain_loss_list}')
-  gain_loss = sum(gain_loss_list)
-
-  gain_loss_100_list = calc_gain_loss_2(predict_df, threshold, 'Y_PRED', 100, False)
-  l_100 = len(list(filter(lambda x: (x < 0), gain_loss_100_list)))
-  w_100 = len(list(filter(lambda x: (x > 0), gain_loss_100_list)))
-  p_100 = len(list(filter(lambda x: (x == 0), gain_loss_100_list)))
-#   print(f'gain_loss_100_list: {w_100}/{l_100}/{p_100}::{len(gain_loss_100_list)}::{gain_loss_100_list}--{predict_df.shape}')
-#   print(f'{predict_df.index}')
-  gain_loss_100 = sum(gain_loss_100_list)
-
-  results.append({'threshold':threshold, 'gain_loss':gain_loss,
-                  'gl_win':w, 'gl_loss':l, 'gl_pass':p,
-                  'gain_loss_100':gain_loss_100,
-                  'gl_win_100':w_100, 'gl_loss_100':l_100, 'gl_pass_100':p_100})
-
-static_results_df = pd.DataFrame(data=results, columns=['threshold', 'gain_loss', 'gl_win', 'gl_loss', 'gl_pass',
-                                                        'gain_loss_100', 'gl_win_100', 'gl_loss_100', 'gl_pass_100'])
-# static_results_df = static_results_df[['threshold', 'gain_loss_100', 'gl_win_100', 'gl_loss_100', 'gl_pass_100']]
-static_results_df = static_results_df.sort_values('gain_loss_100', ascending=False)
-
-static_results_df["gl_win"] = static_results_df["gl_win"].astype(int)
-static_results_df["gl_loss"] = static_results_df["gl_loss"].astype(int)
-static_results_df["gl_pass"] = static_results_df["gl_pass"].astype(int)
-
-static_results_df["gl_win_100"] = static_results_df["gl_win_100"].astype(int)
-static_results_df["gl_loss_100"] = static_results_df["gl_loss_100"].astype(int)
-static_results_df["gl_pass_100"] = static_results_df["gl_pass_100"].astype(int)
-
-
-st.write(static_results_df.head())
 
 '''------------------------------------------------'''
 
-# st.write(f'B Starting: {start_date_string}')
-
-# # Linear regression with sliding window
-# window_sizes = [3, 6, 12]
-# gain_percents = [0, .05, .1, .2]
-
-# result_df = pd.DataFrame(columns=['window', 'percent', 'gain_loss'])
-
-# bet_size = 100
-# for window_size in window_sizes:
-#   for gain_percent in gain_percents:
-
-#     test_df = calc_sliding_window(window_size, btc_month_mean, False)
-#     test_df = append_gain_loss(month_close_df, test_df)
-
-#     # gain_loss = calc_gain_loss(test_df, gain_percent, 'Y_PRED')
-#     # gain_loss_bet_size = calc_gain_loss_2(test_df, gain_percent, 'Y_PRED', bet_size, False)
-
-
-#     gain_loss_list = calc_gain_loss(test_df, gain_percent, 'Y_PRED')
-#     # print(f'gain_loss_list: {gain_loss_list}')
-#     gain_loss = sum(gain_loss_list)
-
-#     gain_loss_bet_size_list = calc_gain_loss_2(test_df, gain_percent, 'Y_PRED', bet_size, False)
-#     # print(f'gain_loss_bet_size_list: {gain_loss_bet_size_list}')
-#     gain_loss_bet_size = sum(gain_loss_bet_size_list)
-
-
-#     loop_df = pd.DataFrame(data=[[window_size, gain_percent, gain_loss, gain_loss_bet_size]], columns=['window', 'percent', 'gain_loss', f'gain_loss_{bet_size}'])
-#     result_df = pd.concat([result_df, loop_df], ignore_index=True)
-
-# sorted_result_df = result_df.sort_values('gain_loss_100', ascending=False)
-# sorted_result_df = sorted_result_df.assign(hack='').set_index('hack')
-# sorted_result_df = sorted_result_df[['window', 'percent', 'gain_loss_100']]
-
-# st.write(sorted_result_df.head())
-
-'''------------------------------------------------'''
-# Multiple regression techniques with sliding window
-
-st.write('C Starting: 2020-01-01')
-a = calc_with_params('2020-01-01', 100)
-# a = a[['window', 'percent', 'model_name', 'gain_loss_100']]
-a.sort_values(by='gain_loss_100', ascending=False)
-
-a["gl_win"] = a["gl_win"].astype(int)
-a["gl_loss"] = a["gl_loss"].astype(int)
-a["gl_pass"] = a["gl_pass"].astype(int)
-
-a["gl_win_100"] = a["gl_win_100"].astype(int)
-a["gl_loss_100"] = a["gl_loss_100"].astype(int)
-a["gl_pass_100"] = a["gl_pass_100"].astype(int)
-
-a = a.assign(hack='').set_index('hack')
+from_date = '2010-01-01'
+st.write(f'Starting: {from_date}')
+a = calc_with_params(from_date, 100)
 st.write(a.head())
 
-# st.write('D Starting: 2021-01-01')
-# b = calc_with_params('2021-01-01', 100)
-# b = b[['window', 'percent', 'model_name', 'gain_loss_100']]
-# b.sort_values(by='gain_loss_100', ascending=False).head()
-# b = b.assign(hack='').set_index('hack')
-# st.write(b.head())
+from_date = '2020-01-01'
+st.write(f'Starting: {from_date}')
+a = calc_with_params(from_date, 100)
+st.write(a.head())
 
-# st.write('E Starting: 2022-01-01')
-# c = calc_with_params('2022-01-01', 100)
-# c = c[['window', 'percent', 'model_name', 'gain_loss_100']]
-# c.sort_values(by='gain_loss_100', ascending=False).head()
-# c = c.assign(hack='').set_index('hack')
-# st.write(c.head())
+from_date = '2021-01-01'
+st.write(f'Starting: {from_date}')
+a = calc_with_params(from_date, 100)
+st.write(a.head())
 
+from_date = '2022-01-01'
+st.write(f'Starting: {from_date}')
+a = calc_with_params(from_date, 100)
+st.write(a.head())
 
 with st.form("my_form"):
 
-    # slider_val = st.slider("Form slider")
-    # checkbox_val = st.checkbox("Form checkbox")
-
-
     from_date = st.text_input('from date YYYY-MM-DD')
-    # invest_amount = st.text_input('Invest Amount')
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
         st.write("from_date", from_date, "invest_amount", 100)
         user_result = calc_with_params(from_date, 100)
-        # user_result = user_result[['window', 'percent', 'model_name', 'gain_loss_100']]
-        user_result.sort_values(by='gain_loss_100', ascending=False).head()
-
-        # pd.to_numeric(user_result[['gl_win', 'gl_loss', 'gl_pass']], downcast='integer')
-        user_result["gl_win"] = user_result["gl_win"].astype(int)
-        user_result["gl_loss"] = user_result["gl_loss"].astype(int)
-        user_result["gl_pass"] = user_result["gl_pass"].astype(int)
-
-        user_result["gl_win_100"] = user_result["gl_win_100"].astype(int)
-        user_result["gl_loss_100"] = user_result["gl_loss_100"].astype(int)
-        user_result["gl_pass_100"] = user_result["gl_pass_100"].astype(int)
-
-
-        user_result = user_result.assign(hack='').set_index('hack')
         st.write(user_result.head())
